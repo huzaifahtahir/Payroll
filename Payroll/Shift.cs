15193dc87e822a66a18b6cab9382e908f82494bc
Huzaifah <tahirhuzaifah@outlook.com>
2020-02-03 12:38:59 -0500

    Add project files.

---
+++ Payroll/Shift.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll
+{
+    class Shift
+    {
+        #region Field Variables
+        private string m_WorkerId;
+        private double m_HoursWorked;
+        private DateTime m_Date;
+        #endregion
+
+        public string WorkerId { get => m_WorkerId; set => m_WorkerId = value; }
+        public double HoursWorked { get => m_HoursWorked; set => m_HoursWorked = value; }
+        public DateTime Date { get => m_Date; set => m_Date = value; }
+
+        public Shift()
+        {
+            m_WorkerId = "R1";
+            m_HoursWorked = 5;
+            m_Date = new DateTime(1994, 06, 12);
+        }
+
+
+    }
+}
---
+++ Payroll/Worker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll
+{
+    class Worker
+    {
+        private string m_Name;
+        private int m_Id;
+        private double m_Payrate;
+
+
+        public int Id { get => m_Id; set => m_Id = value; }
+        public double Payrate { get => m_Payrate; set => m_Payrate = value; }
+        public string Name { get => m_Name; set => m_Name = value; }
+
+        public Worker()
+        {
+            m_Name = "John Doe";
+            m_Id = 1;
+            m_Payrate = 1.11;
+        }
+
+        public override string ToString()
+        {
+            return Name + Id + Payrate;
+        }
+
+    }
+}
